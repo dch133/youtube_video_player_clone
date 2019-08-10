fd6ad26e51bea5be249786418b85e9a603a02a94
unknown <daniel.ch.13@gmail.com>
2019-08-10 18:42:27 -0400

    Updates

---
+++ precache-manifest.29f4e51b05bd90df362381ce80e71e61.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fcc90711c25e8937762d86b33704cb",
+    "url": "/youtube_video_player_clone/index.html"
+  },
+  {
+    "revision": "05341d52d04b7fbfbd46",
+    "url": "/youtube_video_player_clone/static/js/2.37b03199.chunk.js"
+  },
+  {
+    "revision": "89d04c6e918dd4bf0c02",
+    "url": "/youtube_video_player_clone/static/js/main.fc8f7d9c.chunk.js"
+  },
+  {
+    "revision": "9575068960c82b1d4b19",
+    "url": "/youtube_video_player_clone/static/js/runtime~main.bd872fff.js"
+  }
+]);
